0d1f4e49e5b3e9c0bfbb1586361a6668224e9ad9
Prajanya Shrestha <prajanya.stha7@gmail.com>
2023-09-24 21:31:17 +0545

    Header Footer
    Installed react-bootstrap boostrap react-icons

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+    </>
+  );
+};
+
+export default App;
