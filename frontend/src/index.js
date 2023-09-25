db3b13aa6bf06d6246b8ba63267c098034610fd9
Prajanya Shrestha <prajanya.stha7@gmail.com>
2023-09-25 08:46:57 +0545

    Product Details Page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,10 +7,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 );
