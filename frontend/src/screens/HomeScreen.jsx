defa72a03ffbd4db4d4abbef9d2419f0a9066378
Prajanya Shrestha <prajanya.stha7@gmail.com>
2023-09-26 07:53:44 +0545

    Fetch Products from Backend
    Backend still has dummy products.js

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,13 +1,23 @@
 import { Row, Col } from 'react-bootstrap'
-import products from '../products'
 import Product from '../components/Product'
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    }
+    fetchProducts();
+  }, [])
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
         {products.map(product => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id} >
             <Product product={product} />
           </Col>
         ))}
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,21 @@
 import { useParams } from "react-router-dom";
-import products from "../products";
+import { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
 import Rating from "../components/Rating";
-
+import axios from "axios";
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
-  const product = products.find(p => p._id == productId)
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data)
+    }
+    fetchProduct();
+  }, [productId])
+
 
   return (
     <>
