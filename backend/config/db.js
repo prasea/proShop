1143ec26ce681a0abaf7d4a31a5ea0a2f4ad95ec
Prajanya Shrestha <prajanya.stha7@gmail.com>
2023-09-26 15:42:35 +0545

    Modeling Our Data

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose"
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected : ${conn.connection.host}`)
+    console.log(`M  ongoDB Connected : ${conn.connection.host}`)
+    
   } catch (error) {
     console.log(`Error : ${error.message}`)
     process.exit(1)
