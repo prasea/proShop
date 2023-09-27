810fb53c537fdb5c94b58a5d56670ebea8a7f963
Prajanya Shrestha <prajanya.stha7@gmail.com>
2023-09-27 22:13:34 +0545

    Custom Error Middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,8 +10,11 @@ router.get('/', asyncHandler(async (req, res) => {
 }))
 router.get('/:id', asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
-  if(product)
+  if(product){
     res.json(product);
-  res.status(404).json({message : 'Product not found'})
+  } else {
+    res.status(404);
+    throw new Error('Product not found')
+  }
 }))
 export default router;
