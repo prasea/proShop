01695a9feb7ae8a325a0c4e8d1e9241770833e5d
Prajanya Shrestha <prajanya.stha7@gmail.com>
2023-09-28 20:19:12 +0545

    Get Product Details Endpoint Challenge

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -2,14 +2,20 @@ import { PRODUCTS_URL } from "../constats";
 import { apiSlice } from "./apiSlice";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
-  endpoints : builder => ({
-    getProducts : builder.query({
-      query : () => ({
-        url : PRODUCTS_URL
-      }), 
-      keepUnusedDataFor : 5
+  endpoints: builder => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCTS_URL
+      }),
+      keepUnusedDataFor: 5
+    }),
+    getProductsDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`
+      }),
+      keepUnusedDataFor: 5
     })
   })
 })
 
-export const {useGetProductsQuery} = productsApiSlice;
+export const { useGetProductsQuery, useGetProductsDetailsQuery } = productsApiSlice;
