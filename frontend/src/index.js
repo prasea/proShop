c5d5994945cf0d9848cae48e0e9fa2678a61ad26
Prajanya Shrestha <prajanya.stha7@gmail.com>
2023-09-28 12:23:10 +0545

    Redux Store & API Slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,8 @@ import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import { Provider } from 'react-redux'
+import store from './store'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
@@ -19,7 +21,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
