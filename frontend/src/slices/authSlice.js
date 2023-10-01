beceed57668a68d342ab1b44bec7166c0c71642d
Prajanya Shrestha <prajanya.stha7@gmail.com>
2023-10-01 15:36:35 +0545

    Create Reviews : Backend Frontend

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -20,4 +20,4 @@ const authSlice = createSlice({
 })
 
 export default authSlice.reducer;
-export const { setCredentials, logout } = authSlice.actions
+export const { setCredentials, logout } = authSlice.actions 
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -43,8 +43,16 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         url: `${PRODUCTS_URL}/${productId}`,
         method: 'DELETE'
       })
+    }),
+    createReview: builder.mutation({
+      query: data => ({
+        url: `${PRODUCTS_URL}/${data.productId}/reivew`,
+        method: 'POST',
+        body: data
+      }),
+      invalidatesTags: ['Product']
     })
   })
 })
 
-export const { useGetProductsQuery, useGetProductsDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation, useDeleteProductMutation } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductsDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation, useDeleteProductMutation, useCreateReviewMutation } = productsApiSlice;
